1ad833d3b239fec76848186a2e50ddaf28d8d9fb
Marian Boda <marianboda@gmail.com>
2016-05-29 19:14:50 +0200

    Added basic calendar rendering + webpack build

---
+++ index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const monthNames = ['','','','','Máj', 'Jún', 'Júl', 'August']
+
+const dayContainer = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  width: 710
+}
+
+const dayStyle = {
+  flexGrow: 0,
+  flexShrink: 0,
+  width: 100,
+  height: 100,
+  border: '#333 1px solid',
+  marginRight: -1,
+  marginBottom: -1
+}
+
+const monthTitleStyle = {}
+
+function getWeekDay(year, month, day) {
+  let d = new Date(year, month-1, day).getDay()
+  if (d === 0) d = 7
+  return d-1
+}
+
+
+const App = React.createClass({
+  render() {
+    let months = [5,6,7,8]
+    let numOfDays = (year, month) => (new Date(year, month,0)).getDate()
+    let monthBlocks = months.map(i => {
+
+      let days = Array.from(Array(numOfDays(2016,i)).keys())
+      if (i === 5) days = days.filter(i => i >= 27)
+      let dayBlocks = days.map(i => <div style={dayStyle}>{i+1}</div>)
+      let firstDayOfWeek = getWeekDay(2016, i, days[0])
+      if (firstDayOfWeek > 0) {
+        dayBlocks.unshift(<div style={{width: firstDayOfWeek * 101}}></div>)
+      }
+      console.log(2016, i, firstDayOfWeek)
+      return <div>
+        <div style={monthTitleStyle}>{monthNames[i-1].toUpperCase()}</div>
+        <div style={dayContainer}>
+          {dayBlocks}
+        </div>
+      </div>
+    })
+    return (<div style={{width: '100%'}}>
+      <h1>Kalendár</h1>
+      <div>{monthBlocks}</div>
+    </div>)
+    }
+})
+
+ReactDom.render(<App />, document.getElementById('content'))
